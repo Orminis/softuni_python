809df457ef032d6b37b3b268102d5ccbdbb7d1e5
Orminis <k.nedialkov@gmail.com>
2023-10-03 23:49:37 +0300

    Small updates

--- 02.Fundamentals/04.Functions/12.Functions More Exer/04.tribonacci_sequence.py
+++ 02.Fundamentals/04.Functions/12.Functions More Exer/04.tribonacci_sequence.py
@@ -1,19 +1,45 @@
-def printtribrec(n):
-    if n == 0 or n == 1 or n == 2:
-        return 0
-    elif n == 3:
-        return 1
-    else:
-        return (printtribrec(n - 1) +
-                printtribrec(n - 2) +
-                printtribrec(n - 3))
+# def printtribrec(n):
+#     if n == 0 or n == 1 or n == 2:
+#         return 0
+#     elif n == 3:
+#         return 1
+#     else:
+#         return (printtribrec(n - 1) +
+#                 printtribrec(n - 2) +
+#                 printtribrec(n - 3))
+#
+#
+# def printtrib(n):
+#     for i in range(1, n):
+#         print(printtribrec(i), " ", end="")
+#
+#
+# # Driver code
+# n = int(input())
+# printtrib(n)
 
 
-def printtrib(n):
-    for i in range(1, n):
-        print(printtribrec(i), " ", end="")
+# with memory cache optimization
+# fib_cache = {0: 0, 1:1}
+#
+# def fib(n):
+#     if n in fib_cache:  # Base case
+#         return fib_cache[n]
+#
+#     fib_cache[n] = fib(n - 1) + fib(n - 2)  # Recursive case
+#     return fib_cache
+#
+# [fib(n) for n in range(60)]
+# result = [v for v in fib_cache.values()]
+# print(result)
 
+fib_cache = []
 
-# Driver code
-n = int(input())
-printtrib(n)
+n = int(input("Input number: "))
+
+for i in range(n):
+    if i == 0 or i == 1:
+        fib_cache.append(i)
+        continue
+    fib_cache.append(fib_cache[-1] + fib_cache[-2])
+print(fib_cache)
